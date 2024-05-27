51c9584e01a0239c413172de6cdfbbcf67ebdc44
eyasser <eyasser@ntgclarity.com>
2024-05-27 22:34:28 +0300

    create main component

---
+++ src/app/Pages/main/main.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from "../../Shared-Component/header/header.component";
+import {FooterComponent} from "../../Shared-Component/footer/footer.component";
+
+@Component({
+  selector: 'app-main',
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.scss',
+})
+export class MainComponent {
+}
+
+
