3f43c4d7b77637694d55da1df5e85b129c7b4cfa
eyasser <eyasser@ntgclarity.com>
2024-05-27 22:36:50 +0300

    edit routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./Pages/home/home.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login', loadComponent: () => import('./Pages/login/login.component').then((m) => m.LoginComponent),
+  },
+  {
+    path: 'main',
+    loadComponent: () =>
+      import('./Pages/main/main.component').then((m) => m.MainComponent),
+    children: [
+      { path: 'home', component: HomeComponent },
+      { path: '', pathMatch: 'full', redirectTo: 'home'},
+    ],
+  },
+  { path: '**', redirectTo: 'home' },
+
+];
