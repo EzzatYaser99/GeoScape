b731902ce07851dcafc8590a103001278ad32c86
eyasser <eyasser@ntgclarity.com>
2024-05-31 20:11:39 +0300

    impelment contacts component

--- src/app/Pages/contacts/contacts.component.ts
+++ src/app/Pages/contacts/contacts.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import {InputTextareaModule} from "primeng/inputtextarea";
+import {ChipsModule} from "primeng/chips";
+import {ButtonModule} from "primeng/button";
+import {RippleModule} from "primeng/ripple";
 
 @Component({
   selector: 'app-contacts',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    InputTextareaModule,
+    ChipsModule,
+    ButtonModule,
+    RippleModule
+  ],
   templateUrl: './contacts.component.html',
   styleUrl: './contacts.component.scss'
 })
 export class ContactsComponent {
 
+    name:any ;
+  email: any;
+  subject: any;
+  message: any;
 }
