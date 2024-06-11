6c57c924e185789b8432142598f8b761c2e5ed6e
eyasser <eyasser@ntgclarity.com>
2024-06-11 17:12:53 +0300

    complete company profile pdf and do animations

--- GeoScape-Front/src/app/Pages/about/about.component.ts
+++ GeoScape-Front/src/app/Pages/about/about.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import {Button, ButtonDirective} from "primeng/button";
+import {Ripple} from "primeng/ripple";
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [
+    ButtonDirective,
+    Ripple,
+    Button
+  ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
+  showProfilePdf: boolean = false;
 
 }
