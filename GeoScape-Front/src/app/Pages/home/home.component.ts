c316f135ac7dfa239b712ff2916c01e808ee500a
eyasser <eyasser@ntgclarity.com>
2024-06-22 03:32:21 +0300

    implement home page  and edit on services

--- GeoScape-Front/src/app/Pages/home/home.component.ts
+++ GeoScape-Front/src/app/Pages/home/home.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import {HeaderComponent} from "../../Shared-Component/header/header.component";
+import {AvatarModule} from "primeng/avatar";
+import {FieldsetModule} from "primeng/fieldset";
+import {PrimeTemplate} from "primeng/api";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [
-    HeaderComponent
-  ],
+    imports: [
+        HeaderComponent,
+        AvatarModule,
+        FieldsetModule,
+        PrimeTemplate
+    ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
