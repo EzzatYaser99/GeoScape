f077f103deec46ee24eb05def633ba9fe4c70ca1
eyasser <eyasser@ntgclarity.com>
2024-07-08 02:09:49 +0300

    update partners page

--- GeoScape-Front/src/app/Pages/partners/partners.component.ts
+++ GeoScape-Front/src/app/Pages/partners/partners.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import {AvatarModule} from "primeng/avatar";
+import {FieldsetModule} from "primeng/fieldset";
+import {PrimeTemplate} from "primeng/api";
 
 @Component({
   selector: 'app-partners',
   standalone: true,
-  imports: [],
+    imports: [
+        AvatarModule,
+        FieldsetModule,
+        PrimeTemplate
+    ],
   templateUrl: './partners.component.html',
   styleUrl: './partners.component.scss'
 })
