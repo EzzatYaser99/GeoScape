3e85b223cba31151560e457f42b8926f28dfe70f
eyasser <eyasser@ntgclarity.com>
2024-07-15 01:32:36 +0300

    change files document approvals

--- GeoScape-Front/src/app/Pages/home/home.component.ts
+++ GeoScape-Front/src/app/Pages/home/home.component.ts
@@ -28,45 +28,22 @@ export class HomeComponent implements OnInit{
       {
         id: 1, name: 'GEOSCAPE Profile', pdfSrc: 'GEOSCAPE-Profile.pdf',imgSrc: 'GEOSCAPE-Profile.png',
       },
+
       {
-        id: 2, name: 'شهادة العلامة التجارية', pdfSrc: 'شهادة-العلامة-التجارية.pdf',imgSrc: 'شهادة-العلامة-التجارية.png'
-      },
-      {
-        id: 3, name: 'شهادة التوطين', pdfSrc: 'شهادة-التوطين.pdf',imgSrc: 'شهادة-التوطين.png'
-      },
-      {
-        id: 4, name: 'رخصة بلدية التوهج الدائم', pdfSrc: 'رخصة-بلدية-التوهج-الدائم.pdf',imgSrc: 'رخصة-بلدية-التوهج-الدائم.png'
-      },
-      {
-        id: 5, name: 'شهادة التامينات', pdfSrc: 'شهادة-التامينات.pdf',imgSrc: 'شهادة-التامينات.png'
-      },
-      {
-        id: 6, name: 'شهادة اشتراك الغرفة التوهج الدائم', pdfSrc: 'شهادة-اشتراك-الغرفة-التوهج-الدائم.pdf',imgSrc: 'شهادة-اشتراك-الغرفة-التوهج-الدائم.png'
-      },
-      {
-        id: 7, name: 'سجل التوهج للمقاولات', pdfSrc: 'سجل-التوهج-للمقاولات.pdf',imgSrc: 'سجل-التوهج-للمقاولات.png'
-      },
-      {
-        id: 8, name: 'العنوان الوطني العليا التوهج الدائم', pdfSrc: 'العنوان-الوطني-العليا-التوهج-الدائم.pdf',imgSrc: 'العنوان-الوطني-العليا-التوهج-الدائم.png'
-      },
-      {
-        id: 9, name: 'سجل جندلة الرئيسي', pdfSrc: 'سجل-جندلة-الرئيسي.pdf',imgSrc: 'سجل-جندلة-الرئيسي.png'
-      },
-      {
-        id: 10, name: 'شهادة الأيزو', pdfSrc: 'شهادة-الأيزو.pdf',imgSrc: 'شهادة-الأيزو.png'
+        id: 2, name: 'شهادة الأيزو', pdfSrc: 'شهادة-الأيزو.pdf',imgSrc: 'شهادة-الأيزو.png'
       },
 
       {
-        id: 11, name: 'ذا لاين', pdfSrc: 'ذا-لاين.pdf',imgSrc: 'ذا-لاين.png'
+        id: 3, name: 'ذا لاين', pdfSrc: 'ذا-لاين.pdf',imgSrc: 'ذا-لاين.png'
       },
       {
-        id: 12, name: 'مؤسسة المسار الرياضي', pdfSrc: 'مؤسسة-المسار-الرياضي.pdf',imgSrc: 'مؤسسة-المسار-الرياضي.jpeg'
+        id: 4, name: 'مؤسسة المسار الرياضي', pdfSrc: 'مؤسسة-المسار-الرياضي.pdf',imgSrc: 'مؤسسة-المسار-الرياضي.jpeg'
       },
       {
-        id: 13, name: 'Royal Commission for AlUla', pdfSrc: 'Royal-Commission-for-AlUla.pdf',imgSrc: 'Royal-Commission-for-AlUla.png'
+        id: 5, name: 'Royal Commission for AlUla', pdfSrc: 'Royal-Commission-for-AlUla.pdf',imgSrc: 'Royal-Commission-for-AlUla.png'
       },
       {
-        id: 14, name: 'misk city ', pdfSrc: 'misk-city.pdf',imgSrc: 'misk-city.png'
+        id: 6, name: 'misk city ', pdfSrc: 'misk-city.pdf',imgSrc: 'misk-city.png'
       },
 
     ];
