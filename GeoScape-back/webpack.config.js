aff84c43aabb3afe66292eddb3ee9034b0a016c6
eyasser@ntgclarity.com <eyasser@ntgclarity.com>
2024-08-19 23:28:31 +0200

    build backend

--- GeoScape-back/webpack.config.js
+++ GeoScape-back/webpack.config.js
@@ -1,10 +1,27 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-    mode: 'production', // Or 'development' for development mode
-    entry: './index.js',
+    mode: 'production', // Use 'development' if you're in development mode
+    entry: './index.js', // Your entry point
+    target: 'node', // Target node environment
+    resolve: {
+        fallback: {
+            "fs": false, // Node core modules not available in browser
+            "path": require.resolve("path-browserify"),
+            "buffer": require.resolve("buffer/"),
+            "stream": require.resolve("stream-browserify"),
+            "util": require.resolve("util/"),
+            "url": require.resolve("url/")
+        }
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            Buffer: ['buffer', 'Buffer'],
+        })
+    ],
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
     }
 };
