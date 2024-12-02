7b4fd132510af75a476c5798ab9eeb4ce77f1023
NTG <eyasser@ntgclarity.com>
2024-12-02 13:44:15 +0200

    use document services

--- GeoScape-Front/src/app/Component/uploaded-pdf/uploaded-pdf.component.ts
+++ GeoScape-Front/src/app/Component/uploaded-pdf/uploaded-pdf.component.ts
@@ -3,6 +3,7 @@ import {ActivatedRoute} from "@angular/router";
 import {DomSanitizer} from "@angular/platform-browser";
 import {ProgressSpinnerModule} from "primeng/progressspinner";
 import {NgIf} from "@angular/common";
+import {DocumentService} from "../../core/service/document.service";
 
 @Component({
   selector: 'app-uploaded-pdf',
@@ -20,42 +21,9 @@ export class UploadedPdfComponent implements OnInit {
   url: any;
   isLoading: boolean = true;
 
-  constructor(private route: ActivatedRoute ,private _sanitizer :DomSanitizer ) {
-    this.documents = [
-      {
-        id: 1, name: 'GEOSCAPE Profile', pdfSrc: 'GEOSCAPE-Profile.pdf',imgSrc: 'GEOSCAPE-Profile.webp',
-      },
-
-      {
-        id: 2, name: 'ISO certification', pdfSrc: 'شهادة-الأيزو.pdf',imgSrc: 'شهادة-الأيزو.webp'
-      },
-
-      {
-        id: 3, name: 'The Line', pdfSrc: 'ذا-لاين.pdf',imgSrc: 'ذا-لاين.webp'
-      },
-      {
-        id: 4, name: 'Sports Boulevard Foundation', pdfSrc: 'مؤسسة-المسار-الرياضي.pdf',imgSrc: 'مؤسسة-المسار-الرياضي.webp'
-      },
-      {
-        id: 5, name: 'Royal Commission for AlUla', pdfSrc: 'Royal-Commission-for-AlUla.pdf',imgSrc: 'Royal-Commission-for-AlUla.webp'
-      },
-      {
-        id: 6, name: 'misk city ', pdfSrc: 'misk-city.pdf',imgSrc: 'misk-city.webp'
-      },
-      {
-        id: 7, name: 'WA0025', pdfSrc: 'WA0025.pdf',imgSrc: 'WA0025.webp'
-      },
-      {
-        id: 8, name: 'MAT-000057_02', pdfSrc: 'MAT-000057.pdf',imgSrc: 'MAT-000057.webp'
-      },
-      {
-        id: 9, name: 'GR-AUP-PQD-LAN-PLA', pdfSrc: 'GR-AUP-PQD-LAN-PLA.pdf',imgSrc: 'GR-AUP-PQD-LAN-PLA.webp'
-      },
-      {
-        id: 10, name: 'GR-AUP-MAT-LAN', pdfSrc: 'GR-AUP-MAT-LAN.pdf',imgSrc: 'GR-AUP-MAT-LAN.webp'
-      },
-
-    ];
+  constructor(private route: ActivatedRoute ,private _sanitizer :DomSanitizer ,
+              private documentService: DocumentService // Inject the service
+  ) {
 
   }
 
@@ -73,7 +41,7 @@ export class UploadedPdfComponent implements OnInit {
       .snapshot
       .paramMap
       .get('id');
-    this.selectedDocument = this.documents.find((value:any) => value.id == Number(id))
+    this.selectedDocument = this.documentService.getDocumentById(Number(id));
     this.url = 'assets/document-approvals/'+ this.selectedDocument.pdfSrc;
    this.url = this._sanitizer.bypassSecurityTrustResourceUrl(this.url);
 
--- GeoScape-Front/src/app/Pages/home/home.component.ts
+++ GeoScape-Front/src/app/Pages/home/home.component.ts
@@ -4,6 +4,7 @@ import {FieldsetModule} from "primeng/fieldset";
 import {PrimeTemplate} from "primeng/api";
 import {RouterLink} from "@angular/router";
 import {ImageModule} from "primeng/image";
+import {DocumentService} from "../../core/service/document.service";
 
 @Component({
   selector: 'app-home',
@@ -20,45 +21,11 @@ import {ImageModule} from "primeng/image";
 })
 export class HomeComponent implements OnInit{
   documents: any;
-constructor() {
-  this.documents = [
-    {
-      id: 1, name: 'GEOSCAPE Profile', pdfSrc: 'GEOSCAPE-Profile.pdf',imgSrc: 'GEOSCAPE-Profile.webp',
-    },
-
-    {
-      id: 2, name: 'ISO certification', pdfSrc: 'شهادة-الأيزو.pdf',imgSrc: 'شهادة-الأيزو.webp'
-    },
-
-    {
-      id: 3, name: 'The Line', pdfSrc: 'ذا-لاين.pdf',imgSrc: 'ذا-لاين.webp'
-    },
-    {
-      id: 4, name: 'Sports Boulevard Foundation', pdfSrc: 'مؤسسة-المسار-الرياضي.pdf',imgSrc: 'مؤسسة-المسار-الرياضي.webp'
-    },
-    {
-      id: 5, name: 'Royal Commission for AlUla', pdfSrc: 'Royal-Commission-for-AlUla.pdf',imgSrc: 'Royal-Commission-for-AlUla.webp'
-    },
-    {
-      id: 6, name: 'misk city ', pdfSrc: 'misk-city.pdf',imgSrc: 'misk-city.webp'
-    },
-    {
-      id: 7, name: 'WA0025', pdfSrc: 'WA0025.pdf',imgSrc: 'WA0025.webp'
-    },
-    {
-      id: 8, name: 'MAT-000057_02', pdfSrc: 'MAT-000057.pdf',imgSrc: 'MAT-000057.webp'
-    },
-    {
-      id: 9, name: 'GR-AUP-PQD-LAN-PLA', pdfSrc: 'GR-AUP-PQD-LAN-PLA.pdf',imgSrc: 'GR-AUP-PQD-LAN-PLA.webp'
-    },
-    {
-      id: 10, name: 'GR-AUP-MAT-LAN', pdfSrc: 'GR-AUP-MAT-LAN.pdf',imgSrc: 'GR-AUP-MAT-LAN.webp'
-    },
-
-  ];
+constructor(private documentService: DocumentService) {
 
 }
   ngOnInit(): void {
+    this.documents = this.documentService.getDocuments();
 
   }
 
