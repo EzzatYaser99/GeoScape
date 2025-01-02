ddd000f16db7bda45fa57698fb239540a5f2c43d
Ezzat Yaser <eyasser@ntgclarity.com>
2025-01-02 14:30:25 +0200

    update ui and ux 3

--- GeoScape-Front/src/app/Component/prasada-products/prasada-products.component.ts
+++ GeoScape-Front/src/app/Component/prasada-products/prasada-products.component.ts
@@ -7,6 +7,7 @@ import {SeedlingProductsService} from "../../core/service/seedling-products.serv
 import {CardModule} from "primeng/card";
 import {CultivationProductsService} from "../../core/service/cultivation-products.service";
 import {CarouselModule, CarouselResponsiveOptions} from "primeng/carousel";
+import {ImageModule} from "primeng/image";
 
 @Component({
   selector: 'app-prasada-products',
@@ -16,7 +17,8 @@ import {CarouselModule, CarouselResponsiveOptions} from "primeng/carousel";
     GalleriaModule,
     CardModule,
     CarouselModule,
-    Button
+    Button,
+    ImageModule
   ],
   templateUrl: './prasada-products.component.html',
   styleUrl: './prasada-products.component.scss'
